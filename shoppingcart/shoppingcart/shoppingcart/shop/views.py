7bd7420f344f114324c7538f961273039d42a41c
dianaconstantinescu <dianacristinacons@gmail.com>
2018-09-06 20:46:00 +0300

    filter products

--- shoppingcart/shoppingcart/shoppingcart/shop/views.py
+++ shoppingcart/shoppingcart/shoppingcart/shop/views.py
@@ -8,9 +8,15 @@ from .models import Category,Product
 
 def product_list(request):
    products= Product.objects.all()
-   category=Category.objects.all()
-   context={'category':category,'products':products}  #trebuie sa am products in for in template
+   categories=Category.objects.all()
+   context={'category':categories,'products':products}  #trebuie sa am products in for in template
    return render(request, "list.html", context)
+
+def category_product_list(request,categoryid=1):
+    categories=Category.objects.all()
+    products=Product.objects.filter(category=categoryid)
+    context={'category':categories,'products':products}
+    return render(request,"categoryproductlist.html",context)
 
 def product_detail(request,productid):
     id=productid
